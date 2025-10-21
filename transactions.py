de97363340c680b84a861ffd1c3025f733f0a7a4
AhmedHossam8 <ahmedhossamemara8@gmail.com>
2025-10-22 00:18:42 +0300

    Added transaction viewing feature

--- transactions.py
+++ transactions.py
@@ -16,3 +16,18 @@ def add_transaction(username, amount, currency, category, date, description, typ
     transactions.append(transaction)
     save_transactions(transactions)
     print("Transaction saved successfully!")
+    
+def view_transactions(username):
+    transactions = load_transactions()
+    user_transactions = [t for t in transactions if t["username"] == username]
+
+    if not user_transactions:
+        print("No transactions found for this user.")
+        return
+    
+    print("id | amount | currency | category | date | description | type")
+    print()
+    
+    for t in user_transactions:
+        print(f"{t['id']:<5} | {t['amount']:<10} | {t['currency']:<8} | {t['category']:<12} | {t['date']:<12} | {t['description']:<20} | {t['type']}")
+    
